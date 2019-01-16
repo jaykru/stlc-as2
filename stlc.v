5c528369f5f0f4c6c2f0c126fb41875f6aa6c2ed
Jay Kruer <kruerj@reed.edu>
2019-01-16 16:45:39 -0500

    spurce up notations

--- stlc.v
+++ stlc.v
@@ -18,44 +18,43 @@ Inductive value {n} : tm n -> Prop :=
 Hint Constructors value.
 
 (* evaluation relation *)
-
+Reserved Notation "t --> t'" (at level 50).
 Inductive red {n} : tm n -> tm n -> Prop :=
-| red_beta : forall (t : tm (S n)) (t' : tm n) (T: ty),
-    red (app (lam T t) t') (t [t' ..])
-| red_app_1 : forall t1 t1' t2,
-    red t1 t1' ->
-    red (app t1 t2) (app t1' t2)
-| red_app_2 : forall t1 t2 t2',
-    red t2 t2' ->
-    red (app t1 t2) (app t1 t2')
-| red_tif_disc : forall t1 t1' t2 t3,
-    red t1 t1' ->
-    red (tif t1 t2 t3) (tif t1' t2 t3)
-| red_tif_true : forall t2 t3,
-    red (tif ttrue t2 t3) t2
-| red_tif_false : forall t2 t3,
-    red (tif tfalse t2 t3) t3.
+| red_beta : forall (t : tm (S n)) (t' : tm n) (T: ty), (app (lam T t) t') --> (t [t' ..])
+| red_app_1 : forall t1 t1' t2, t1 --> t1' ->
+                                (app t1 t2) --> (app t1' t2)
+| red_app_2 : forall t1 t2 t2', t2 --> t2' -> 
+                                (app t1 t2) --> (app t1 t2')
+| red_tif_disc : forall t1 t1' t2 t3, t1 --> t1' -> 
+                                      (tif t1 t2 t3) --> (tif t1' t2 t3)
+| red_tif_true : forall t2 t3, (tif ttrue t2 t3) --> t2
+| red_tif_false : forall t2 t3, (tif tfalse t2 t3) --> t3
+where " t --> t' " := (red t t').
+                   
 
 Hint Constructors red.
 
 (* typing relation *)
+Reserved Notation " Gamma ||- t : T " (at level 60, t at level 70).
 Inductive hasType {n} : ctxt n -> tm n -> ty -> Prop :=
-| T_var: forall G i,
-    hasType G (var_tm i) (G i)
-| T_app : forall G t1 t2 T1 T2,
-    hasType G t1 (arrow T1 T2) ->
-    hasType G t2 T1 ->
-    hasType G (app t1 t2) T2
-| T_abs : forall G t T1 T2,
-    @hasType (S n) (T1 .: G) t T2 (* use stream cons defined by as2 to implement context bindings *)
-    -> hasType G (lam T1 t) (arrow T1 T2) 
-| T_ttrue : forall G, hasType G ttrue bool
-| T_tfalse : forall G, hasType G ttrue bool
-| T_tif : forall G t1 t2 t3 T, 
-    hasType G t1 bool ->
-    hasType G t2 T ->
-    hasType G t3 T ->
-    hasType G (tif t1 t2 t3) T.
+| T_var: forall Gamma i,
+    Gamma ||- (var_tm i) : (Gamma i)
+| T_app : forall Gamma t1 t2 T1 T2,
+    Gamma ||- t1 : (arrow T1 T2) ->
+    Gamma ||- t2 : T1 ->
+    Gamma ||- (app t1 t2) : T2
+| T_abs : forall Gamma t T1 T2,
+    @hasType (S n) (T1 .: Gamma) t T2 -> (* use stream cons defined by as2 to implement context bindings *)
+    Gamma ||- (lam T1 t) : (arrow T1 T2)
+| T_ttrue : forall Gamma, Gamma ||- ttrue : bool
+| T_tfalse : forall Gamma, Gamma ||- ttrue : bool
+| T_tif : forall Gamma t1 t2 t3 T, 
+    Gamma ||- t1 : bool ->
+    Gamma ||- t2 : T ->
+    Gamma ||- t3 : T ->
+    Gamma ||- (tif t1 t2 t3) : T
+where " Gamma ||- t : T " := (hasType Gamma t T).
+                           
 
 Hint Constructors hasType.
 
@@ -66,32 +65,32 @@ Ltac crunch :=
          | [ f : fin 0 |- _ ] => destruct f
          (* deal with some nonsense that crops up during induction and inversion on our typing relation *) 
          | [ t1 : tm 0 |- _] => match goal with                            
-                          | [ HTy : hasType empty t1 _ , H : forall t0 : tm 0,
-                                  0 = 0 -> t1 ~= t0 -> forall T : ty, hasType empty t0 T -> value t0 \/ (exists t' : tm 0, red t0 t') |- _] => specialize (H _ eq_refl JMeq_refl _ HTy)
+                          | [ HTy : empty ||- t1 : _ , H : forall t0 : tm 0,
+                                  0 = 0 -> t1 ~= t0 -> forall T : ty, empty ||- t0 : T -> value t0 \/ (exists t' : tm 0, t0 --> t') |- _] => specialize (H _ eq_refl JMeq_refl _ HTy)
                            end
          end.
 
 Ltac inv H := inversion H; crunch; subst; clear H.
 
-Theorem canonical_forms_abs: forall n G (t : tm n) T1 T2,
-    hasType G t (arrow T1 T2) ->
+Theorem canonical_forms_abs: forall n Gamma (t : tm n) T1 T2,
+    Gamma ||- t : (arrow T1 T2) ->
     value t ->
     exists t', t = lam T1 t'.
-  intros n G t T1 T2 HTy Hval.
+  intros n Gamma t T1 T2 HTy Hval.
   induction Hval; inv HTy; exists t; reflexivity.
 Qed.
 
-Theorem canonical_forms_bool: forall n G (t : tm n),
-    hasType G t bool ->
+Theorem canonical_forms_bool: forall n Gamma (t : tm n),
+    Gamma ||- t : bool ->
     value t ->
     (t = ttrue) \/ (t = tfalse).
-  intros n G t HTy Hval.
+  intros n Gamma t HTy Hval.
   induction Hval; inv HTy; auto.
 Qed.
 
 Lemma progress: forall t T,
-      hasType empty t T ->
-      value t \/ exists t', red t t'.
+      empty ||- t : T ->
+      value t \/ exists t', t --> t'.
   Proof.
     intros t T HTy.
     dependent induction t; eauto; crunch.
@@ -129,8 +128,8 @@ Lemma progress: forall t T,
         exists (tif t1' t2 t3); eauto.        
   Qed.
   
-  Fixpoint typres_ren n m G D t T (H : hasType G t T) (delta : fin n -> fin m) {struct H}:
-    (forall f, G f = D (delta f)) -> (hasType D t⟨delta⟩ T).
+  Fixpoint typres_ren n m Gamma Delta t T (H : Gamma ||- t : T) (delta : fin n -> fin m) {struct H}:
+    (forall f, Gamma f = Delta (delta f)) -> (Delta ||- t⟨delta⟩ : T).
     destruct H; simpl; intros; asimpl; try rewrite (H i); econstructor; eauto; try eapply typres_ren; try assumption.
     - exact H.
     - intro.
@@ -142,8 +141,8 @@ Lemma progress: forall t T,
   Qed.
 
   (* CLEAN ME *)
-  Fixpoint typres_subst n m (sigma : fin n -> tm m) (Gamma : ctxt n) (Delta : ctxt m) (t : tm n) T (H: hasType Gamma t T) {struct H} : 
-(forall i, hasType Delta (sigma i) (Gamma i)) -> hasType Delta t[sigma] T.
+  Fixpoint typres_subst n m (sigma : fin n -> tm m) (Gamma : ctxt n) (Delta : ctxt m) (t : tm n) T (H: Gamma ||- t : T) {struct H} : 
+(forall i, Delta ||- (sigma i) : (Gamma i)) -> Delta ||- t[sigma] : T.
   Proof with eauto.
     intros.
     destruct H; asimpl...
@@ -161,10 +160,10 @@ Lemma progress: forall t T,
     econstructor.
   Qed.
 
-  Theorem typres_beta : forall n (G : ctxt n) t t' T' T,
-      hasType (T' .: G) t T ->
-      hasType G t' T' ->
-      hasType G (t [t' ..]) T.
+  Theorem typres_beta : forall n (Gamma : ctxt n) t t' T' T,
+      (T' .: Gamma) ||- t : T ->
+      Gamma ||- t' : T' ->
+      Gamma ||- (t [t' ..]) : T.
   Proof.
     intros.
     eapply typres_subst.
@@ -175,8 +174,8 @@ Lemma progress: forall t T,
   Qed.
 
   (* preservation *)
-  Theorem preservation t t' T (H: hasType empty t T) (Hred: red t t') : 
-    hasType empty t' T.
+  Theorem preservation t t' T (H: empty ||- t : T) (Hred: t --> t') : 
+    empty ||- t' : T.
     induction H.
     - inversion Hred.
     - inversion Hred; subst.
