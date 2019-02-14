a73f6820ddbc8fd9975714a6b8fbf9b019217bae
Jay Kruer <kruerj@reed.edu>
2019-02-14 13:21:39 -0500

    some more automation

--- stlc.v
+++ stlc.v
@@ -2,8 +2,9 @@ Require Import Coq.Logic.Eqdep_dec.
 Require Import Coq.Arith.Peano_dec.
 Require Import Coq.Program.Equality. (* for dependent induction *)
 Require Import as2.
+
 (* contexts *)
-Definition ctxt n := fin n -> ty. (* this definition of a context probably lets us more readily use autosubst's notation and substitution machinery *)
+Definition ctxt n := fin n -> ty. (* this definition of a context lets us more readily use autosubst's notation and substitution machinery *)
 
 Definition empty : ctxt 0 := fun x : fin 0 => let t := match x return ty with
                                                        end in t.
@@ -30,7 +31,6 @@ Inductive red {n} : tm n -> tm n -> Prop :=
 | red_tif_true : forall t2 t3, (tif ttrue t2 t3) --> t2
 | red_tif_false : forall t2 t3, (tif tfalse t2 t3) --> t3
 where " t --> t' " := (red t t').
-                   
 
 Hint Constructors red.
 
@@ -54,23 +54,10 @@ Inductive hasType {n} : ctxt n -> tm n -> ty -> Prop :=
     Gamma ||- t3 : T ->
     Gamma ||- (tif t1 t2 t3) : T
 where " Gamma ||- t : T " := (hasType Gamma t T).
-                           
 
 Hint Constructors hasType.
 
-(* catch-all automation tactic *)
-Ltac crunch :=
-  repeat match goal with
-         (* fin 0 is uninhabited; this immediately solves the goal. *)
-         | [ f : fin 0 |- _ ] => destruct f
-         (* deal with some nonsense that crops up during induction and inversion on our typing relation *) 
-         | [ t1 : tm 0 |- _] => match goal with                            
-                          | [ HTy : empty ||- t1 : _ , H : forall t0 : tm 0,
-                                  0 = 0 -> t1 ~= t0 -> forall T : ty, empty ||- t0 : T -> value t0 \/ (exists t' : tm 0, t0 --> t') |- _] => specialize (H _ eq_refl JMeq_refl _ HTy)
-                           end
-         end.
-
-Ltac inv H := inversion H; crunch; subst; clear H.
+Ltac inv H := inversion H; subst; clear H.
 
 Theorem canonical_forms_abs: forall n Gamma (t : tm n) T1 T2,
     Gamma ||- t : (arrow T1 T2) ->
@@ -88,56 +75,63 @@ Theorem canonical_forms_bool: forall n Gamma (t : tm n),
   induction Hval; inv HTy; auto.
 Qed.
 
-Lemma progress: forall t T,
-      empty ||- t : T ->
-      value t \/ exists t', t --> t'.
-  Proof.
-    intros t T HTy.
-    dependent induction t; eauto; crunch.
-    -
-      (* t is an application *)
-      right.
-      inv HTy.
-      destruct IHt1 as [HValt1 | Hredt1].
-      + (* t1 is a value *)
-        destruct IHt2 as [HValt2 | Hredt2].
-        * (* t2 also value *)
-          assert (exists t0, t1 = lam T1 t0).
-          eapply canonical_forms_abs; eauto.
-          destruct H as [t0 Heq].
-          exists (t0 [t2 ..]).
-          subst.
-          eauto. 
-        * (* t2 steps *)
-          inversion Hredt2.
-          exists (app t1 x).
-          eauto. 
-      +  (* t1 steps *)
-        inversion Hredt1.
-        exists (app x t2); eauto.
-    -
-      inv HTy.
-      right.
-      destruct IHt1 as [HVal | HStep].
-      + (* discriminant of branch is a value *)
-        assert (Ht1: t1 = ttrue \/ t1 = tfalse) by (eapply canonical_forms_bool; eauto).
-        destruct Ht1; subst.
-        * (* branch left *) exists t2; eauto.
-        * (* branch right *) exists t3; eauto.
-      + destruct HStep as [t1' Hredt1].
-        exists (tif t1' t2 t3); eauto.        
-  Qed.
+(* catch-all automation tactic; not great, but works *)
+Ltac crunch :=
+  try match goal with
+         | [t : tm _ |- _] => match goal with
+                            | [ HTy : _ ||- t : (arrow _ _) , Hval : value t |- _] => pose proof (canonical_forms_abs _ _ _ _ _ HTy Hval)
+                            end
+
+         | [t : tm _ |- _] => match goal with
+                            | [ HTy : _ ||- t : bool , Hval : value t |- _] => pose proof (canonical_forms_bool _ _ _ HTy Hval)
+                            end
+         (* fin 0 is uninhabited; this immediately solves the goal. *)
+         | [ f : fin 0 |- _ ] => destruct f; crunch
+         (* deal with some nonsense that crops up during induction and inversion on our typing relation *) 
+         | [ t1 : tm 0 |- _] => match goal with                            
+                          | [ HTy : empty ||- t1 : _ , H : forall t0 : tm 0,
+                                  0 = 0 -> t1 ~= t0 -> forall T : ty, empty ||- t0 : T -> value t0 \/ (exists t' : tm 0, t0 --> t') |- _] => specialize (H _ eq_refl JMeq_refl _ HTy); crunch
+                              end
+         end.
+
+Lemma progess: forall t T,
+    empty ||- t : T ->
+    value t \/ exists t', t --> t'.
+Proof.
+  intros t T HTy.
+  dependent induction t; try inv HTy; eauto; crunch.
+  - (* t is an application *)
+    right.
+    destruct IHt1 as [Hvalt1 | Hstept1].
+    + (* t1 is a value *)
+      destruct IHt2 as [Hvalt2 | Hstept2].
+      * (* t2 is also value *)
+        crunch.
+        destruct H as [t0 Heq]. (* AUTOMATEME *)
+        exists (t0 [t2 ..]).
+        subst.
+        eauto.
+      * (* t2 steps *)
+        inv Hstept2; exists (app t1 x); eauto.
+    + (* t1 steps *)
+      inv Hstept1; exists (app x t2); eauto.
+  - (* t is an if *)
+    right.
+    destruct IHt1 as [Hvalt1 | Hstept1].
+    + (* discriminant of if is a value *)
+      crunch.
+      destruct H; subst. (* AUTOMATEME *)
+      * (* branch left *)
+        exists t2; auto.
+      * (* branch right *)
+        exists t3; auto.
+    + destruct Hstept1 as [t1' Hred].
+      exists (tif t1' t2 t3); eauto.
+Qed.
   
   Fixpoint typres_ren n m Gamma Delta t T (H : Gamma ||- t : T) (delta : fin n -> fin m) {struct H}:
     (forall f, Gamma f = Delta (delta f)) -> (Delta ||- t⟨delta⟩ : T).
-    destruct H; simpl; intros; asimpl; try rewrite (H i); econstructor; eauto; try eapply typres_ren; try assumption.
-    - exact H.
-    - intro.
-      destruct f.
-      simpl.
-      exact (H0 f).
-      simpl.
-      auto.
+    destruct H; simpl; intros; asimpl; try rewrite (H i); eauto; econstructor; try eapply typres_ren; try assumption; try eauto; try intro; try destruct f; try simpl; try eauto.
   Qed.
 
   (* CLEAN ME *)
@@ -145,8 +139,11 @@ Lemma progress: forall t T,
 (forall i, Delta ||- (sigma i) : (Gamma i)) -> Delta ||- t[sigma] : T.
   Proof with eauto.
     intros.
+    (* everything but the lambda case is immediate by substitution and the definitions *)
     destruct H; asimpl...
-    (* lambda case *)
+    (* lambda case follows by structural induction on terms, our
+    substitution rules, and by preservation of types under
+    renamings. *)
     econstructor.
     eapply typres_subst.
     exact H.
@@ -155,7 +152,7 @@ Lemma progress: forall t T,
     destruct i.
     simpl.
     asimpl.
-    eapply typres_ren; eauto.
+    eapply typres_ren...
     simpl.
     econstructor.
   Qed.
@@ -169,8 +166,7 @@ Lemma progress: forall t T,
     eapply typres_subst.
     exact H.
     intro.
-    destruct i; eauto.
-    asimpl; simpl; eauto.
+    destruct i; asimpl; simpl; eauto.
   Qed.
 
   (* preservation *)
